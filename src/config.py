ac6547dcfbf6a4dcad86812a8557cd332f0380d5
Daniel <daniel@sense-ai.co>
2024-12-03 16:51:52 -0500

    partialImprovements

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
 from src import process_data as data_process
 
+FC= 10 #Cutoff frequency in Hz
+WINDOWING = 0.03 #s time of the window // Ej: 0.2s = 200 ms
+OVERLAPPING = 50 #% overlapping percentage
+ELECTRODES = 8 #Number of electrodes used in the experiment  // Incrementar a 10
+THRESHOLD = 60 # Characterization percentage
+
 URLS = {
     1:"https://ninapro.hevs.ch/files/DB1/Preprocessed/s", #1.zip
     2:"https://ninapro.hevs.ch/files/DB2_Preproc/DB2_s", #1.zip
@@ -18,17 +24,17 @@ OUTPUT_DIRS = {
     9:"Ninapro/DB9",
 }
 
-SUBJECTS = {
-    1:27,
-    2:40,
-    3:11
+DATABASE_INFO = {
+    'DB1': {'frequency': 100, 'subjects': 27, 'repetitions': 10},
+    'DB2': {'frequency': 2000, 'subjects': 40, 'repetitions': 6},
+    'DB3': {'frequency': 2000, 'subjects': 11, 'repetitions': 6},
+    'DB4': {'frequency': 2000, 'subjects': 10, 'repetitions': 6},
+    'DB5': {'frequency': 200, 'subjects': 10, 'repetitions': 6},
+    'DB6': {'frequency': 2000, 'subjects': 10, 'repetitions': 6},
+    'DB7': {'frequency': 2000, 'subjects': 22, 'repetitions': 6},
+    'DB8': {'frequency': 2000, 'subjects': 12, 'repetitions': 6},
+    'DB9': {'frequency': 2000, 'subjects': 77, 'repetitions': 6}
 }
-
-DATABASES = {
-    'DB1': 2000,
-    'DB2': 2000,
-    'DB3': 2000
-}#Databases with sampling frequencies in Hz
 
 FEATURES = {
     'RMS_E': data_process.calculate_rms,
@@ -41,7 +47,7 @@ FEATURES = {
 }
 
 MOVEMENTS_LABEL = {
-    '0' : 'Base',
+    '0' : 'Rest',
     '1' : 'Thumb up',
     '2' : 'Extension index and middle',
     '3' : 'Flexion ring and little',
@@ -51,23 +57,23 @@ MOVEMENTS_LABEL = {
     '7' : 'Pointing Index',
     '8' : 'Adduction extended fingers',
     '9' : 'Wrist supination MF',
-    '10' : 'Wrist pronation MF',
-    '11' : 'Wrist supination LF',
-    '12' : 'Wrist pronation LF',
-    '13' : 'Wrist flexion',
-    '14' : 'Wrist extension',
-    '15' : 'Wrist radial deviation',
-    '16' : 'Wrist ulnar deviation',
-    '17' : 'Wrist extension hand closed',
-    '18' : 'Large diameter',
-    '19' : 'Small diameter',
-    '20' : 'Fixed hook',
-    '21' : 'Index finger extension',
-    '22' : 'Medium Wrap',
-    '23' : 'Ring', 
-    '24' : 'Prismastic four fingers',
-    '25' : 'Stick',
-    '26' : 'Writing Tripod',
+    '10': 'Wrist pronation MF',
+    '11': 'Wrist supination LF',
+    '12': 'Wrist pronation LF',
+    '13': 'Wrist flexion',
+    '14': 'Wrist extension',
+    '15': 'Wrist radial deviation',
+    '16': 'Wrist ulnar deviation',
+    '17': 'Wrist extension hand closed',
+    '18': 'Large diameter',
+    '19': 'Small diameter',
+    '20': 'Fixed hook',
+    '21': 'Index finger extension',
+    '22': 'Medium Wrap',
+    '23': 'Ring', 
+    '24': 'Prismastic four fingers',
+    '25': 'Stick',
+    '26': 'Writing Tripod',
     '27': 'Power Sphere',
     '28': 'Three finger sphere',
     '29': 'precision sphere',
@@ -91,18 +97,18 @@ MOVEMENTS_LABEL = {
     '47': 'StrFlexion Index and Little',
     '48': 'StrFlexion Ring and middle',
     '49': 'StrExtension Index and Thumb',
-    '50' : 'Index flexion',
-    '51' : 'Index Extension',
-    '52' : 'Middle flexion',
-    '53' : 'Middle Extension',
-    '54' : 'Ring flexion',
-    '55' : 'Ring Extension',
-    '56' : 'Little flexion',
-    '57' : 'Little Extension',
-    '58' : 'Thumb flexion',
-    '59' : 'Thumb Adduction',
-    '60' : 'Thumb Adbuction',
-    '61' : 'Thumb Extension',
+    '50': 'Index flexion',
+    '51': 'Index Extension',
+    '52': 'Middle flexion',
+    '53': 'Middle Extension',
+    '54': 'Ring flexion',
+    '55': 'Ring Extension',
+    '56': 'Little flexion',
+    '57': 'Little Extension',
+    '58': 'Thumb flexion',
+    '59': 'Thumb Adduction',
+    '60': 'Thumb Adbuction',
+    '61': 'Thumb Extension',
 }
 
 """
@@ -117,9 +123,3 @@ MOVEMENTS_LABEL = {
 }
 """
 
-FC= 10 #Cutoff frequency in Hz
-WINDOWING = 0.03 #s time of the window // Ej: 0.2s = 200 ms
-OVERLAPPING = 50 #% overlapping percentage
-REPETITIONS = 10 #Number of times a movement is repeated
-ELECTRODES = 8 #Number of electrodes used in the experiment
-THRESHOLD = 60 # Characterization percentage
